73e4166871145416ea4412ff9bca0f22414349e5
Israel Nazareth <israelsnazareth@gmail.com>
2023-03-14 23:47:57 -0300

    refact: renames colors

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,11 +9,11 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${({ theme }) => theme['yellow-dark']};
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['yellow-500']};
   }
 
   body {
-    background-color: ${({ theme }) => theme.background};
+    background-color: ${({ theme }) => theme['background-100']};
     -webkit-font-smoothing: antialiased;
   }
 
