3a6f9c914974230967c9a70e5fb2fe3101f6c6f6
Israel Nazareth <israelsnazareth@gmail.com>
2023-09-20 18:03:38 -0300

    :poop: theme type any

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,11 +9,10 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    /* box-shadow: 0 0 0 2px ${({ theme }) => theme['yellow-500']}; */
   }
 
   body {
-    background-color: ${({ theme }) => theme['background-100']};
+    background-color: ${({ theme }: any) => theme['background-100']};
     -webkit-font-smoothing: antialiased;
   }
 
