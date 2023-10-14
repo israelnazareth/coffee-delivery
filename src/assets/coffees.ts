d9eec73f48daa285352d2fd3af859fdc0af89582
Israel <israelsnazareth@gmail.com>
2023-10-14 15:08:38 -0300

    feat: creates coffees data and rename images

---
+++ src/assets/coffees.ts
@@ -0,0 +1,104 @@
+export const coffees = [
+  {
+    image: 'coffees/Expresso.png',
+    tags: ['Tradicional'],
+    title: 'Expresso Tradicional',
+    description: 'O tradicional café feito com água quente e grãos moídos',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Americano.png',
+    tags: ['Tradicional'],
+    title: 'Expresso Americano',
+    description: 'Expresso diluído, menos intenso que o tradicional',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/ExpressoCremoso.png',
+    tags: ['Tradicional'],
+    title: 'Expresso Cremoso',
+    description: 'Café expresso tradicional com espuma cremosa',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/ExpressoGelado.png',
+    tags: ['Tradicional', 'Gelado'],
+    title: 'Expresso Gelado',
+    description: 'Bebida preparada com café expresso e cubos de gelo',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/CaféComLeite.png',
+    tags: ['Tradicional', 'Com leite'],
+    title: 'Café com Leite',
+    description: 'Meio a meio de expresso tradicional com leite vaporizado',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Latte.png',
+    tags: ['Tradicional', 'Com leite'],
+    title: 'Latte',
+    description:
+      'Uma dose de café expresso com o dobro de leite e espuma cremosa',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Capuccino.png',
+    tags: ['Tradicional', 'Com leite'],
+    title: 'Capuccino',
+    description:
+      'Bebida com canela feita de doses iguais de café, leite e espuma',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Macchiato.png',
+    tags: ['Tradicional', 'Com leite'],
+    title: 'Macchiato',
+    description:
+      'Café expresso misturado com um pouco de leite quente e espuma',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Mocaccino.png',
+    tags: ['Tradicional', 'Com leite'],
+    title: 'Mocaccino',
+    description: 'Café expresso com calda de chocolate, pouco leite e espuma',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/ChocolateQuente.png',
+    tags: ['Especial', 'Com leite'],
+    title: 'Chocolate Quente',
+    description: 'Bebida feita com chocolate dissolvido no leite quente e café',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Cubano.png',
+    tags: ['Especial', 'Alcoólico', 'Gelado'],
+    title: 'Cubano',
+    description:
+      'Drink gelado de café expresso com rum, creme de leite e hortelã',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Havaiano.png',
+    tags: ['Especial'],
+    title: 'Havaiano',
+    description: 'Bebida adocicada preparada com café e leite de coco',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Árabe.png',
+    tags: ['Especial'],
+    title: 'Árabe',
+    description: 'Bebida preparada com grãos de café árabe e especiarias',
+    price: 9.9,
+  },
+  {
+    image: 'coffees/Irlandês.png',
+    tags: ['Especial', 'Alcoólico'],
+    title: 'Irlandês',
+    description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
+    price: 9.9,
+  },
+]
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,9 +4,14 @@ import { HeroContainer, HeroImage } from './styles'
 
 export function Home() {
   return (
-    <HeroContainer>
-      <HeroImage src={image} alt="" />
-      <HeroTexts />
-    </HeroContainer>
+    <>
+      <HeroContainer>
+        <HeroImage src={image} alt="" />
+        <HeroTexts />
+      </HeroContainer>
+      <div className="coffee-container">
+        <h2>Nossos cafés</h2>
+      </div>
+    </>
   )
 }
