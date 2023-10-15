946c834b1a714130f68e5da480149cccf681e49d
Israel Nazareth <israelsnazareth@gmail.com>
2023-10-14 23:58:01 -0300

    fix: move images from src to public

--- src/assets/coffees.ts
+++ src/assets/coffees.ts
@@ -1,41 +1,41 @@
 export const coffees = [
   {
-    image: 'src/assets/coffees/Expresso.png',
+    image: './assets/images/Expresso.png',
     tags: ['Tradicional'],
     title: 'Expresso Tradicional',
     description: 'O tradicional café feito com água quente e grãos moídos',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Americano.png',
+    image: './assets/images/Americano.png',
     tags: ['Tradicional'],
     title: 'Expresso Americano',
     description: 'Expresso diluído, menos intenso que o tradicional',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/ExpressoCremoso.png',
+    image: './assets/images/ExpressoCremoso.png',
     tags: ['Tradicional'],
     title: 'Expresso Cremoso',
     description: 'Café expresso tradicional com espuma cremosa',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/ExpressoGelado.png',
+    image: './assets/images/ExpressoGelado.png',
     tags: ['Tradicional', 'Gelado'],
     title: 'Expresso Gelado',
     description: 'Bebida preparada com café expresso e cubos de gelo',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/CaféComLeite.png',
+    image: './assets/images/CaféComLeite.png',
     tags: ['Tradicional', 'Com leite'],
     title: 'Café com Leite',
     description: 'Meio a meio de expresso tradicional com leite vaporizado',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Latte.png',
+    image: './assets/images/Latte.png',
     tags: ['Tradicional', 'Com leite'],
     title: 'Latte',
     description:
@@ -43,7 +43,7 @@ export const coffees = [
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Capuccino.png',
+    image: './assets/images/Capuccino.png',
     tags: ['Tradicional', 'Com leite'],
     title: 'Capuccino',
     description:
@@ -51,7 +51,7 @@ export const coffees = [
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Macchiato.png',
+    image: './assets/images/Macchiato.png',
     tags: ['Tradicional', 'Com leite'],
     title: 'Macchiato',
     description:
@@ -59,21 +59,21 @@ export const coffees = [
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Mocaccino.png',
+    image: './assets/images/Mocaccino.png',
     tags: ['Tradicional', 'Com leite'],
     title: 'Mocaccino',
     description: 'Café expresso com calda de chocolate, pouco leite e espuma',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/ChocolateQuente.png',
+    image: './assets/images/ChocolateQuente.png',
     tags: ['Especial', 'Com leite'],
     title: 'Chocolate Quente',
     description: 'Bebida feita com chocolate dissolvido no leite quente e café',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Cubano.png',
+    image: './assets/images/Cubano.png',
     tags: ['Especial', 'Alcoólico', 'Gelado'],
     title: 'Cubano',
     description:
@@ -81,21 +81,21 @@ export const coffees = [
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Havaiano.png',
+    image: './assets/images/Havaiano.png',
     tags: ['Especial'],
     title: 'Havaiano',
     description: 'Bebida adocicada preparada com café e leite de coco',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Árabe.png',
+    image: './assets/images/Árabe.png',
     tags: ['Especial'],
     title: 'Árabe',
     description: 'Bebida preparada com grãos de café árabe e especiarias',
     price: 9.9,
   },
   {
-    image: 'src/assets/coffees/Irlandês.png',
+    image: './assets/images/Irlandês.png',
     tags: ['Especial', 'Alcoólico'],
     title: 'Irlandês',
     description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
