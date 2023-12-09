57a217033ed22a0b8766062c9d1ba93a10fdea02
Israel Nazareth <israelsnazareth@gmail.com>
2023-12-09 00:33:37 -0300

    refact: change DefaultTheme type to interface and move interfaces to types file

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,64 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+/* eslint-disable prettier/prettier */
 import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-  export type DefaultTheme = ThemeType
-}
-
-export interface State {
-  id: number
-  nome: string
-  sigla: string
-  regiao: {
-    id: number
-    nome: string
-    sigla: string
-  }
-}
-
-export interface UF {
-  id: number
-  sigla: string
-  nome: string
-  regiao?: UF
-}
-
-export interface Mesorregiao {
-  id: number
-  nome: string
-  UF: UF
-}
-
-export interface RegiaoImediata {
-  id: number
-  nome: string
-  'regiao-intermediaria': Mesorregiao
-}
-
-export interface Microrregiao {
-  id: number
-  nome: string
-  mesorregiao: Mesorregiao
-}
-
-export interface Municipio {
-  id: number
-  nome: string
-  microrregiao: Microrregiao
-  'regiao-imediata': RegiaoImediata
-}
-
-export interface City {
-  id: number
-  nome: string
-  municipio: Municipio
-}
-
-export interface ResultsProps {
-  long_name: string | undefined
-  short_name: string | undefined
-  types: string[]
-  formatted_address: string
+  export interface DefaultTheme extends ThemeType { }
 }
